c60c86b3430b675804b5136d84f361158f72fb3c
tharunsuresh-code <70054755+tharunsuresh-code@users.noreply.github.com>
2021-02-22 21:15:04 +0530

    Update intersection_and_union.py
    Corrected NOT operation

--- intersection_and_union.py
+++ intersection_and_union.py
@@ -1,67 +1,85 @@
 
 
 
-def intersection_op(pos_list_1,pos_list2):
+def intersection_op(pos_list_1,pos_list2, NOT):
     output = list()
     top_1 =0
     top_2 = 0
     comparisons = 0
 
-    while top_1<len(pos_list_1) and top_2 < len(pos_list2):
-        comparisons+=1
-        if pos_list_1[top_1]==pos_list2[top_2]:
+    if NOT:
+        while top_1<len(pos_list_1) and top_2 < len(pos_list2):
+            comparisons+=1
+            if pos_list_1[top_1]!=pos_list2[top_2] and pos_list_1[top_1] <pos_list2[top_2]:
+                output.append(pos_list_1[top_1])
+                top_1+=1
+            elif pos_list_1[top_1] >pos_list2[top_2]:
+                top_2+=1
+            else:
+                top_1+=1
+                top_2+=1
+        while top_1 < len(pos_list_1):
             output.append(pos_list_1[top_1])
             top_1+=1
-            top_2+=1
-        elif pos_list_1[top_1] <pos_list2[top_2]:
-            top_1+=1
-        else:
-            top_2+=1
+    else:
+        while top_1<len(pos_list_1) and top_2 < len(pos_list2):
+            comparisons+=1
+            if pos_list_1[top_1]==pos_list2[top_2]:
+                output.append(pos_list_1[top_1])
+                top_1+=1
+                top_2+=1
+            elif pos_list_1[top_1] <pos_list2[top_2]:
+                top_1+=1
+            else:
+                top_2+=1
 
     return output,comparisons
 
 
-def AND_operator(postings):
+def AND_operator(postings, NOT):
     comparison_accumulator =0
     initial_output = postings[0]
     for posting in postings[1:]:
-        output, comparisons = intersection_op(initial_output,posting)
+        output, comparisons = intersection_op(initial_output,posting, NOT)
         comparison_accumulator+=comparisons
     return output, comparison_accumulator
 
 
-def Union_op(pos_list_1,pos_list_2):
+def Union_op(pos_list_1,pos_list_2, NOT):
     output = list()
     top_1 =0
     top_2 = 0
     comparisons = 0
 
-    while top_1<len(pos_list_1) and top_2 < len(pos_list_2):
-        comparisons+=1
-        if pos_list_1[top_1]==pos_list_2[top_2]:
-            output.append(pos_list_1[top_1])
-            top_1+=1
-            top_2+=1
-        elif top_1<len(pos_list_1) and pos_list_1[top_1] <pos_list_2[top_2]:
-            output.append(pos_list_1[top_1])
-            top_1+=1
-        else:
-            top_2+=1
-            output.append(pos_list_2[top_2])
+    if NOT:
+        output = pos_list_2
+    else:
+        while top_1<len(pos_list_1) and top_2 < len(pos_list_2):
+            comparisons+=1
+            if pos_list_1[top_1]==pos_list_2[top_2]:
+                output.append(pos_list_1[top_1])
+                top_1+=1
+                top_2+=1
+            elif top_1<len(pos_list_1) and pos_list_1[top_1] <pos_list_2[top_2]:
+                output.append(pos_list_1[top_1])
+                top_1+=1
+            else:
+                top_2+=1
+                output.append(pos_list_2[top_2])
 
-    while top_1 < len(pos_list_1):
-        output.append(pos_list_1[top_1])
-        top_1+=1
-    while top_2 < len(pos_list_2):
-        output.append(pos_list_2[top_2])
-        top_2+=1
+        while top_1 < len(pos_list_1):
+            output.append(pos_list_1[top_1])
+            top_1+=1
+        while top_2 < len(pos_list_2):
+            output.append(pos_list_2[top_2])
+            top_2+=1
     return output,comparisons    
 
-def OR_operator(postings):
+def OR_operator(postings, NOT):
     comparison_accumulator =0
     initial_output = postings[0]
     for posting in postings[1:]:
-        output, comparisons = Union_op(initial_output,posting)
+        output, comparisons = Union_op(initial_output,posting, NOT)
         comparison_accumulator+=comparisons
     return output, comparison_accumulator
 if __name__=="__main__":
@@ -95,25 +113,29 @@ if __name__=="__main__":
         comparisons_sum = 0
         for index,operator in enumerate(operators):
             pos_lists = []
-
+            NOT = False
+            
+            '''
             if "NOT" in operator:
                 not_items = inverted_index.get(query_terms[index+1])                
                 pos_list_2 = [x for x in all_docids if x not in not_items]
             else:
                 pos_list_2 = inverted_index.get(query_terms[index+1])
-                
+            '''
             if "AND" in operator:
                 if index ==0:
                     pos_list_1 = inverted_index.get(query_terms[index])
                 else:
                     pos_list_1  = output
                 print("pos_list_1",pos_list_1)
-                #pos_list_2 = inverted_index.get(query_terms[index+1])
+                pos_list_2 = inverted_index.get(query_terms[index+1])
+                if "NOT" in operator:
+                    NOT = True
                 pos_list_1.sort()
                 pos_list_2.sort()
                 pos_lists.append(pos_list_1)
                 pos_lists.append(pos_list_2)
-                output,comparisons = AND_operator(pos_lists)
+                output,comparisons = AND_operator(pos_lists, NOT)
                 comparisons_sum+=comparisons
                 print("Number of comparisons",comparisons_sum)
                 print("The merged postings are", output)
@@ -122,12 +144,16 @@ if __name__=="__main__":
                             pos_list_1 = inverted_index.get(query_terms[index])
                         else:
                             pos_list_1  = output
-                        #pos_list_2 = inverted_index.get(query_terms[index+1])
+                        pos_list_2 = inverted_index.get(query_terms[index+1])
+                        if "NOT" in operator:
+                            NOT = True
+                            not_items = inverted_index.get(query_terms[index+1])                
+                            pos_list_2 = [x for x in all_docids if x not in not_items]
                         pos_list_1.sort()
                         pos_list_2.sort()
                         pos_lists.append(pos_list_1)
                         pos_lists.append(pos_list_2)
-                        output,comparisons = OR_operator(pos_lists)
+                        output,comparisons = OR_operator(pos_lists, NOT)
                         comparisons_sum+=comparisons
                         print("Number of comparisons",comparisons_sum)
                         print("The merged postings are", output)
